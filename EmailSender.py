2b7aa65f015c6f593feddc11a1e44ca56da6ca51
Prabhat Verma <117444677+Prabhat708@users.noreply.github.com>
2023-11-25 00:26:45 +0530

    Add files via upload
    This project is a Python script leveraging Gmail's SMTP server to send emails. This code enables users to input recipient email addresses, subject, message content, and their Gmail credentials via the terminal. The script encapsulates the email sending process in a function, handling message composition and connection to the SMTP server. Note: For security reasons, avoid storing passwords directly in code.

---
+++ EmailSender.py
@@ -0,0 +1,36 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def send_email(subject, message, from_email, to_emails, username, password):
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['To'] = ', '.join(to_emails)
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(message, 'plain'))
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(username, password)
+        server.sendmail(from_email, to_emails, msg.as_string())
+        server.quit()
+        print("Email sent successfully!")
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    
+
+    emails = input("Enter email id's whom you want to send emails separated by space:\n")
+    from_email = "kapil902677@gmail.com"
+    to_emails = emails.split() 
+    username = "kapil902677@gmail.com"
+    
+    
+    subject = input("Write subject of mails.\n")
+    message = input("Write your mail content here..........\n")
+    password = input("Enter your Password\n")
+    
+    send_email(subject, message, from_email, to_emails, username, password)
